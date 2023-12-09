28ab0fcb492c73b164b5df4ee30f075b77ed8460
VishnuWorld <vrbhandari1995@gmail.com>
2023-12-09 22:39:46 +0530

    added text in text file, added var in setting

--- first_project/settings.py
+++ first_project/settings.py
@@ -141,3 +141,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+# added below code -- vishnu
+VAR = 10
